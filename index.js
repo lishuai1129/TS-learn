704a2f818eace576cf94817645635c261675d17c
lishuai1129 <2728701176@qq.com>
2025-11-12 17:06:43 +0800

    zhuyi

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-
-let welcmoe = 'hello'
-welcmoe()
-
-// 逻辑漏洞
-const str = Date.now() % 2 ? '奇数' : '偶数'
-if (str !== '奇数') {
-  alert('hello')
-} else if (str === '偶数') {
-  alert('world')
-}
-
-const obj = { width: 10, height: 15 };
-const area = obj.width * obj.heigth;
-
-const message = 'hello,world'
-message.toUperCase()
+"use strict";
+let str1;
+str1 = 'hello';
+str1 = new String('hello'); //报错
+let str2;
+str2 = 'hello';
+str2 = new String('hello');
+console.log(typeof str1);
+console.log(typeof str2);
